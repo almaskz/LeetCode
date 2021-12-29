0331e5f9f86d76e5f5b1786d8e75b2c05a75e215
almaskz <almas.kairatuly@gmail.com>
2021-12-29 10:26:22 -0800

    Time: 11 ms (5.73%), Space: 13.7 MB (95.04%) - LeetHub

---
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.swift
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        guard let node = root else { return [] }
+        
+        let left = preorderTraversal(node.left)
+        let right = preorderTraversal(node.right)
+        return [node.val] + left + right
+    }
+}
