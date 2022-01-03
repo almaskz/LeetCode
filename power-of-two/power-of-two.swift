9bc341829db484c1c5a12077448cf2f29c37f788
almaskz <almas.kairatuly@gmail.com>
2022-01-03 12:14:56 -0800

    Time: 22 ms (5.15%), Space: 13.4 MB (99.70%) - LeetHub

--- power-of-two/power-of-two.swift
+++ power-of-two/power-of-two.swift
@@ -2,9 +2,31 @@ class Solution {
     func isPowerOfTwo(_ n: Int) -> Bool {
         if n == 0 { return false }
         if n == 1 { return true }
-        if n % 2 == 0 {
-            return isPowerOfTwo(n/2)
-        }
-        return false
+        return (n & (n-1)) == 0 
+        // if n % 2 == 0 {
+        //     return isPowerOfTwo(n/2)
+        // }
+        // return false
+        
+//         var num = n
+        
+//         while num % 2 == 0 {
+//             num /= 2
+//             if num == 1 {
+//                 return true
+//             }
+//         }
+//         return false
+        
+        // 3
+        // 011 = 3 
+        // 010 = 2
+        // 010 = 2
+        
+        // 8 
+        // 1000 = 8
+        // 0111 = 7
+        // 0000 = 0
+        //(n & (n - 1)) == 0
     }
 }
