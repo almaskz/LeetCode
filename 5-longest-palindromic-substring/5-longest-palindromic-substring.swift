a152c79df73489ca86a4f7b7ac39e67ea1a435be
almaskz <almas.kairatuly@gmail.com>
2022-01-07 12:17:55 -0800

    Time: 156 ms (44.44%), Space: 15.5 MB (5.56%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.swift
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.swift
@@ -1,23 +1,23 @@
 class Solution {
     func longestPalindrome(_ s: String) -> String {
         var n = s.count
+        var dp = Array(repeating: Array(repeating: false, count: n), count: n)
+        var aL = 0
+        var aR = 0
         var arr = Array(s)
-        var memo = Array(repeating: Array(repeating: false, count: n), count: n)
-        var aLeft = 0
-        var aRight = 0
         
         for i in stride(from: n-1, through: 0, by: -1) {
             for j in i..<n {
-                if arr[i] == arr[j] && 
-                    (j-i < 3 || memo[i+1][j-1]) {
-                        memo[i][j] = true
-                        if j - i > aRight - aLeft {
-                            aLeft = i
-                            aRight = j
-                        } 
+                if arr[j] == arr[i] && (j-i < 3 || dp[i+1][j-1]) {
+                    dp[i][j] = true
+                    if j-i > aR-aL {
+                        aL = i
+                        aR = j
+                    }
                 }
             }
         }
-        return String(arr[aLeft...aRight])
+        
+        return String(arr[aL...aR])
     }
 }
