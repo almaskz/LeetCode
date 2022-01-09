38e2255c565375e22a64966f6a2f29166dc593d6
almaskz <almas.kairatuly@gmail.com>
2022-01-08 21:27:08 -0800

    Time: 16 ms (100.00%), Space: 14.2 MB (38.10%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func finalPrices(_ prices: [Int]) -> [Int] {
+        guard prices.count > 1 else { return prices }
+        
+        //[8 4 6 2 3]
+        // 
+        
+        var ans = [Int]()
+        ans.reserveCapacity(prices.count)
+        var discount = 0
+        for i in 0..<prices.count {
+            discount = 0
+            for j in stride(from: i+1, to: prices.count, by: 1) {
+                if prices[j] <= prices[i] {
+                    discount = prices[j]
+                    break
+                }     
+            }
+            ans.append(prices[i] - discount)
+        }
+        return ans
+    }
+}
