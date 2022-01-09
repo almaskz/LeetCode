0833bec6aa52080d775db23eae69d432d30a9290
almaskz <almas.kairatuly@gmail.com>
2022-01-08 21:59:27 -0800

    Time: 16 ms (100.00%), Space: 13.9 MB (100.00%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.swift
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.swift
@@ -2,22 +2,30 @@ class Solution {
     func finalPrices(_ prices: [Int]) -> [Int] {
         guard prices.count > 1 else { return prices }
         
-        //[8 4 6 2 3]
+        //  0  1 2 3
+        // [10,1,1,6]
+        //         i
+        // stack = [23] 
         // 
+        // 
+        // [9 0 1 6]
         
-        var ans = [Int]()
-        //ans.reserveCapacity(prices.count)
-        var discount = 0
+        var ans = prices
+        return stackSolution(&ans)
+    }
+    
+    func stackSolution(_ prices: inout [Int]) -> [Int] {
+        var stack = [Int]() // indecies
+        stack.reserveCapacity(prices.count)
+        
         for i in 0..<prices.count {
-            discount = 0
-            for j in stride(from: i+1, to: prices.count, by: 1) {
-                if prices[j] <= prices[i] {
-                    discount = prices[j]
-                    break
-                }     
+            while !stack.isEmpty && prices[stack.last!] >= prices[i] {
+                let index = stack.removeLast()
+                prices[index] -= prices[i]
             }
-            ans.append(prices[i] - discount)
+            stack.append(i)
         }
-        return ans
+        
+        return prices
     }
 }
