94e406babec38610f0d4d3eafe295114da5a6fdc
almaskz <almas.kairatuly@gmail.com>
2022-01-13 13:53:13 -0800

    Time: 1326 ms (32.69%), Space: 17.4 MB (84.27%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.swift
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.swift
@@ -1,20 +1,14 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        //  0 1 2 3 4 5
-        // [7,1,5,3,6,4]
-        //            i 
-        // stack = [7 1]
-        // max = 5
-        
         var curMin = prices[0]
-        var curMax = 0
+        var maxDiff = 0
         for i in 1..<prices.count {
             if prices[i] > curMin {
-                curMax = max(curMax, prices[i] - curMin)
+                maxDiff = max(maxDiff, prices[i] - curMin)
             } else {
-                curMin = prices[i]
+                curMin = prices[i]   
             }
         }
-        return curMax
+        return maxDiff
     }
 }
