052491fb402e1724500a391e8285e4d1bd9e4bbd
almaskz <almas.kairatuly@gmail.com>
2022-01-17 16:18:36 -0800

    Time: 50 ms (23.28%), Space: 14.3 MB (74.03%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.swift
+++ 152-maximum-product-subarray/152-maximum-product-subarray.swift
@@ -1,22 +1,16 @@
 class Solution {
     func maxProduct(_ nums: [Int]) -> Int {
         //  [2, 3, -2, 4]
-        //dp[2  6  -2  4]
-        //dp[-0 2   6  6]
-        //  [-2,0,-1]
-        //dp[-2 0  0]
-        //dp[-0-2  0]
         var result = nums.max()!
-        
         var curMin = 1
         var curMax = 1
         var temp = 0
         for i in nums {
-            if i == 0 {
-                curMin = 1
-                curMax = 1
-                continue
-            }
+            // if i == 0 {
+            //     curMin = 1
+            //     curMax = 1
+            //     continue
+            // }
             temp = i*curMin
             curMin = min(i*curMax, i*curMin, i)
             curMax = max(i*curMax, temp, i)
