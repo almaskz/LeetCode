c364af06ee0636f07eb6007806d59cff22894c84
almaskz <almas.kairatuly@gmail.com>
2022-01-21 15:12:59 -0800

    Time: 1473 ms (21.02%), Space: 18.2 MB (76.99%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.swift
+++ 53-maximum-subarray/53-maximum-subarray.swift
@@ -1,41 +1,17 @@
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        let r = nums.count-1
-        let mid = (0+r)/2
+        //[5,4,-1,7,8]
+        //   i
+        // cur = 
+        // max = 5
+        var result = nums[0]
         
-        return maxSubArray(nums, 0, r)
-    }
-    
-    func maxSubArray(_ arr: [Int], _ l: Int, _ r: Int) -> Int {
-        // base case 
-        if l >= r {
-             return arr[l]
+        var cur = result
+        for i in 1..<nums.count {
+            cur = max(cur+nums[i], nums[i])
+            result = max(cur, result)
         }
         
-        let m = (l+r)/2 
-        
-        let left = maxSubArray(arr, l, m)
-        let right = maxSubArray(arr, m+1, r)
-        let center = maxCrossing(arr, l, r, m)
-        
-        return max(left, right, center)
-    }
-    
-    func maxCrossing(_ arr: [Int], _ l: Int, _ r: Int, _ m: Int) -> Int {
-        var maxLeft = arr[m]
-        var cur = maxLeft
-        for i in stride(from: m-1, through: l, by: -1) {
-            cur += arr[i]
-            maxLeft = max(maxLeft, cur)
-        }
-        
-        var maxRight = arr[m+1] 
-        cur = maxRight
-        for i in stride(from: m+2, through: r, by: 1) {
-            cur += arr[i]
-            maxRight = max(maxRight, cur)
-        }
-        
-        return maxLeft+maxRight
+        return result
     }
 }
