6e36a2679421ad1f5661e9d629e43e9db29312cb
almaskz <almas.kairatuly@gmail.com>
2022-01-21 14:08:57 -0800

    Time: 92 ms (38.69%), Space: 14.5 MB (27.18%) - LeetHub

--- 1-two-sum/1-two-sum.swift
+++ 1-two-sum/1-two-sum.swift
@@ -1,16 +1,22 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        // naive O(n^2)
-        // hash O(n)
-        // 
+        //nums = [2,7,11,15], target = 9
+        // 9-7 = 2 
+        // [2,7,11,15]
+        //    i 
+        // [2] = 0
+        // []
+        
         var dict = [Int: Int]()
         for i in 0..<nums.count {
-            if let index = dict[target - nums[i]] {
+            let diff = target - nums[i]
+            if let index = dict[diff] {
                 return [i, index]
             } else {
                 dict[nums[i]] = i
             }
         }
-        return []
+        
+        return [0,0]
     }
 }
