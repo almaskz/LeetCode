e2a4a9981dfc1057f0d5fd3f483abb0e32b1d1b6
almaskz <almas.kairatuly@gmail.com>
2022-01-21 13:59:37 -0800

    Time: 238 ms (43.97%), Space: 20.2 MB (90.66%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.swift
+++ 238-product-of-array-except-self/238-product-of-array-except-self.swift
@@ -1,25 +1,24 @@
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        // nums =  [-1  1  0  -3  3]
-        //result =   1 -1 -1   0  0
-        //                       0 
-        //result = 24 12  8  6
+        //1 [1,2,3,4] p = 6
+        //         i
+        //  [1 1 2 6]
+        //        <-r
+        // [24 12 4 1]
+        //  24 12 8 6
         
-        var result = Array(repeating: 1, count: nums.count)        
-        var prefix = 1
-        // nums =   -1  1  0  -3  3
-        //result =   1  1  1   1  1
-        //              i
-        // prefix = 1
+        var result = Array(repeating: 1, count: nums.count)
+        
+        var product = 1
         for i in 0..<nums.count {
-            result[i] = prefix
-            prefix *= nums[i]
+            result[i] = product
+            product *= nums[i]
         }
         
-        var postfix = 1
+        product = 1
         for i in stride(from: nums.count-1, through: 0, by: -1) {
-            result[i] *= postfix
-            postfix *= nums[i]
+            result[i] *= product
+            product *= nums[i]
         }
         
         return result
