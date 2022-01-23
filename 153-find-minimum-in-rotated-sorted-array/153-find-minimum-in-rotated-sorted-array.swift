d8aa8bbb2d6ed9c2ea7093336749d10d9cd59847
almaskz <almas.kairatuly@gmail.com>
2022-01-22 19:51:39 -0800

    Time: 16 ms (79.04%), Space: 14.3 MB (18.90%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.swift
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.swift
@@ -5,29 +5,22 @@ class Solution {
         //[5,1,2,3,4]
         // l   m   r
         //
-        //[4 ]
-        
-        
-        // if l > m => search left
-        // if l < m => search right
-        // if l < r => min = l
-        // 
         
         var result = nums[0]
         var l = 0, r = nums.count-1
         while l <= r {
             if nums[l] < nums[r] {
                 return min(result, nums[l])
-            }
-            let m = (l+r)/2
-            if nums[l] > nums[m] {
-                // search left
-                result = min(result, nums[m])
-                r = m-1
             } else {
-                // search right
-                result = min(result, nums[l])
-                l = m+1
+                let m = (l+r)/2
+                result = min(result, nums[m])
+                if nums[l] > nums[m] {
+                    // search left
+                    r = m-1
+                } else {
+                    // search right                    
+                    l = m+1
+                }   
             }
         }
         
