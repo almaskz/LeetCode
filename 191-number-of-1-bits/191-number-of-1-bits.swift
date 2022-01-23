652b01af0d6a4a9f1140dd680842feccde072599
almaskz <almas.kairatuly@gmail.com>
2022-01-23 11:22:33 -0800

    Time: 3 ms (71.11%), Space: 13.2 MB (80.00%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.swift
+++ 191-number-of-1-bits/191-number-of-1-bits.swift
@@ -1,11 +1,13 @@
 class Solution {
     func hammingWeight(_ n: Int) -> Int {
+        // 0100
+        // 0001
+        // 0001
         var count = 0
         var a = n
         while a != 0 {
-            a &= (a-1) 
-            count += 1
-            
+            count += a&1
+            a = a >> 1
         }
         
         return count
