5d681006ffba4f5239cdc6798496abe0ad22cdc1
almaskz <almas.kairatuly@gmail.com>
2022-01-23 10:22:02 -0800

    Time: 4 ms (24.29%), Space: 13.6 MB (88.57%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.swift
+++ 371-sum-of-two-integers/371-sum-of-two-integers.swift
@@ -23,9 +23,7 @@ class Solution {
         var a = a 
         var b = b
         while b != 0 {
-            let temp = a
-            a ^= b
-            b = (b&temp)<<1
+            (a, b) = (a^b, (a&b)<<1)
         }
         
         return a
