e4b6f5079ca7b54c847fe9139900f72765b5e3ef
almaskz <almas.kairatuly@gmail.com>
2022-01-24 13:27:40 -0800

    Time: 1257 ms (34.62%), Space: 18.2 MB (76.62%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.swift
+++ 53-maximum-subarray/53-maximum-subarray.swift
@@ -1,22 +1,44 @@
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        // 
-        //[-2 1 -3 4 -1 2 1 -5 4]
-        //                     i
-        // 
-        // cur = max(nums[i], cur + nums[i]) 5
-        // max = max(cur, max)               6 
-        
-        // 
-        
-        var cur = nums[0]
-        var result = cur
-        
-        for i in 1..<nums.count {
-            cur = max(nums[i], cur + nums[i])
-            result = max(cur, result)
+        // 0  1  2 3  4 5
+        //[-3 4 -1 2 1 -5]
+        //  l         m        r
+        //[-2 1 -3 4]  []
+        //[-2] left = -2
+        //    [1 -3 4]
+        //    [1] = left = 1
+        //      [-3 4] 
+        return maxSubArray(0, nums.count-1, nums)
+    }
+    
+    func maxSubArray(_ l: Int, _ r: Int, _ arr: [Int]) -> Int {
+        // base case 
+        if l >= r {
+            return arr[l]
         }
         
-        return result
+        let m = (l+r)/2
+        let left = maxSubArray(l, m, arr)
+        let right = maxSubArray(m+1, r, arr)
+        let mid = countMid(l, m, r, arr)
+        
+        return max(left, right, mid)
+    }
+    
+    func countMid(_ l: Int, _ m: Int, _ r: Int, _ arr: [Int]) -> Int {
+        var cur = arr[m]
+        var resultLeft = cur
+        for i in stride(from: m-1, through: l, by: -1) {
+            cur += arr[i]
+            resultLeft = max(cur, resultLeft)
+        }
+        
+        cur = arr[m+1]
+        var resultRight = cur
+        for i in stride(from: m+2, through: r, by: 1) {
+            cur += arr[i]
+            resultRight = max(resultRight, cur)
+        }
+        return resultLeft+resultRight
     }
 }
