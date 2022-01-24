af41180fc5cd9a475db08dc34bc6c7867aa5564e
almaskz <almas.kairatuly@gmail.com>
2022-01-24 12:42:27 -0800

    Time: 1331 ms (31.33%), Space: 18.3 MB (76.62%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.swift
+++ 53-maximum-subarray/53-maximum-subarray.swift
@@ -1,14 +1,19 @@
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        //[5,4,-1,7,8]
-        //          i
-        // cur = 23
-        // max = 23
-        var result = nums[0]
+        // 
+        //[-2 1 -3 4 -1 2 1 -5 4]
+        //                     i
+        // 
+        // cur = max(nums[i], cur + nums[i]) 5
+        // max = max(cur, max)               6 
         
-        var cur = result
+        // 
+        
+        var cur = nums[0]
+        var result = cur
+        
         for i in 1..<nums.count {
-            cur = max(cur+nums[i], nums[i])
+            cur = max(nums[i], cur + nums[i])
             result = max(cur, result)
         }
         
