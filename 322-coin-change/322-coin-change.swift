d34e5ad6f4a8b10fa7e9f40d74e2cdf50108365d
almaskz <almas.kairatuly@gmail.com>
2022-01-25 14:29:03 -0800

    Time: 58 ms (35.98%), Space: 13.6 MB (99.58%) - LeetHub

--- 322-coin-change/322-coin-change.swift
+++ 322-coin-change/322-coin-change.swift
@@ -1,33 +1,34 @@
 class Solution {
     func coinChange(_ coins: [Int], _ amount: Int) -> Int {
-        //[1,2,5] amount = 6
-        //
-        // f(n) = the fewest number of coins needed to make n {}
-        // f(0) = {} = 0 
-        // f(1) = {1} = 1 
-        // f(2) = {1 1} or {2}  = 1 
-        // f(3) = {1 1 1} or {2 1} = 2
-        // f(4) = {1111} or {2 1 1} or {2 2} = 2
-        // f(5) = {11111} or {2111} or {221} or {5} = 1
-        // f(6) = {5 1} = 2
-        // f(7) = {5 2} = 2 = 
+        var memo = Array(repeating: Int.max, count: amount+1)
+        memo[0] = 0
+        var target = amount
+        return numberOfCoins(coins, &target, &memo)
+    }
+    
+    func numberOfCoins(_ coins:[Int], _ target: inout Int, _ memo: inout [Int]) -> Int {
+        // base case 
+        if target < 0 {
+            return Int.max
+        }
+        if target == 0 {
+            return 0
+        }
         
-        var dp = Array(repeating: amount+1, count: amount+1)
-        dp[0] = 0
+        if memo[target] != Int.max {
+            return memo[target]
+        }
         
-        //  0 1 2 3 4 5
-        // [0 1 1 2 6 6]
-        //        i
-        // [1 2 5] 
-        // coin = 1
-        for i in stride(from: 1, through: amount, by: 1) {
-            for coin in coins {
-                if i - coin >= 0 {
-                    dp[i] = min(dp[i], 1 + dp[i-coin])    
-                }
+        var curMin = Int.max
+        for coin in coins {
+            var diff = target - coin
+            let result = numberOfCoins(coins, &diff, &memo)
+            if result >= 0 && result < curMin {
+                curMin = 1 + result   
             }
         }
         
-        return dp[amount] == amount+1 ? -1 : dp[amount]
+        memo[target] = curMin == Int.max ? -1 : curMin
+        return memo[target]
     }
 }
