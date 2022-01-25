5467f4111eda3e7d926faab9188429049ca8b8ff
almaskz <almas.kairatuly@gmail.com>
2022-01-25 15:37:07 -0800

    Time: 18 ms (28.57%), Space: 14.7 MB (11.22%) - LeetHub

--- 133-clone-graph/133-clone-graph.swift
+++ 133-clone-graph/133-clone-graph.swift
@@ -13,21 +13,21 @@
 class Solution {
     func cloneGraph(_ node: Node?) -> Node? {
         var dict = [Int: Node]()
-        
-        func dfs(_ node: Node?) -> Node? {
-            guard let node = node else { return nil }
-            if let copy = dict[node.val] {
-                return copy
-            } else {
-                let copy = Node(node.val)
-                dict[node.val] = copy
-                for nei in node.neighbors {
-                    copy.neighbors.append(dfs(nei))
-                }
-                return copy
-            }
-        }
-        
-        return dfs(node)
+        return dfs(node, &dict)
     }
+    
+    func dfs(_ node: Node?, _ dict: inout [Int: Node]) -> Node? {
+        guard let node = node else { return nil }
+        if let copy = dict[node.val] {
+            return copy
+        } else {
+            let copy = Node(node.val)
+            dict[node.val] = copy
+            for nei in node.neighbors {
+                copy.neighbors.append(dfs(nei, &dict))
+            }
+            return copy
+        }
+    }
+    
 }
