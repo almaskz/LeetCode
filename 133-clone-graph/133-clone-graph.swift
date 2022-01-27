9f54476bdebd737fa1995d3d3092483fbdd9757d
almaskz <almas.kairatuly@gmail.com>
2022-01-27 11:15:51 -0800

    Time: 8 ms (96.28%), Space: 14.3 MB (44.26%) - LeetHub

--- 133-clone-graph/133-clone-graph.swift
+++ 133-clone-graph/133-clone-graph.swift
@@ -15,7 +15,6 @@ class Solution {
         // will hold copies of the nodes 
         var visited = [Int: Node]() 
         return dfs(node, &visited)
-        //return bfs(node)
     }
     
     func dfs(_ node: Node?, _ visited: inout [Int: Node]) -> Node?{
@@ -36,39 +35,4 @@ class Solution {
         
         return visited[node.val]
     }
-    
-    func bfs(_ node: Node?) -> Node? {
-        guard let node = node else { return nil }
-        
-        var visited = [Int: Node]()     // holds copies of nodes
-        var stack = [Node]()            // holds original nodes
-        
-        stack.append(node)              
-        visited[node.val] = Node(node.val)
-        
-        while stack.isEmpty == false {
-            let curNode = stack.removeFirst()
-            
-            for nei in curNode.neighbors {
-                guard let nei = nei else { continue }
-                
-                // we already created copy for this node
-                if let copy = visited[nei.val] {
-                    // just append that copy to the neighbors of curNode
-                    visited[curNode.val]!.neighbors.append(copy) 
-                } else {
-                    // we need to create a fresh copy
-                    let copy = Node(nei.val)
-                    // assign that copy to as visited
-                    visited[nei.val] = copy
-                    // append that copy to neighbors of curNode
-                    visited[curNode.val]!.neighbors.append(copy)
-                    // add nei to the stack to seach its neighbors
-                    stack.append(nei)
-                }
-            }
-        }
-        return visited[node.val]
-    }
-    
 }
