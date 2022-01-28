054d800e5b018a2125d8957f073be5a9721fe644
almaskz <almas.kairatuly@gmail.com>
2022-01-28 10:58:11 -0800

    Time: 444 ms (27.40%), Space: 16.7 MB (50.00%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.swift
+++ 200-number-of-islands/200-number-of-islands.swift
@@ -7,11 +7,33 @@ class Solution {
             for j in 0..<arr[i].count {
                 if arr[i][j] == Character("1") {
                     numOfIslands += 1 
-                    dfs(i, j, &arr)
+                    //dfs(i, j, &arr)
+                    bfs(i, j, &arr)
                 }
             }
         }
         return numOfIslands
+    }
+    
+    func bfs(_ x: Int, _ y: Int, _ grid: inout [[Character]]) {
+        var stack = [[Int]]()
+        stack.append([x, y])
+    
+        while stack.isEmpty == false {
+            var point = stack.removeLast()
+            let pointX = point[0]
+            let pointY = point[1]
+            grid[pointX][pointY] = Character("0")
+            for dir in [(0,1), (1,0), (0,-1), (-1,0)] {
+                let row = pointX + dir.0
+                let colm = pointY + dir.1
+                if  row >= 0 && row < grid.count &&
+                    colm >= 0 && colm < grid[0].count && 
+                    grid[row][colm] == Character("1") {
+                        stack.append([row, colm])
+                }
+            }
+        }
     }
     
     func dfs(_ x: Int, _ y: Int, _ grid: inout [[Character]]) {
