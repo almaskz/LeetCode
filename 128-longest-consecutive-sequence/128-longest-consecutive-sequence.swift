b69eedb8dd3f19d575f23b2726609fd8637d2324
almaskz <almas.kairatuly@gmail.com>
2022-01-27 19:07:55 -0800

    Time: 555 ms (25.17%), Space: 17.3 MB (84.62%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        //[100,4,200,1,3,2]
+        //           i
+        // nextNum = 5
+        // count = 4
+        // longSeq = 4
+        // [ .. 1234 ... 100 ... 200]
+        guard nums.count > 0 else { return 0 }
+        
+        let set = Set(nums)
+        var longSeq = 0
+        
+        for i in nums {
+            // check if its a start of the new sequence (i-1 should not exist in our set)
+            // if i-1 does not exist in our set its a start of the new sequence 
+            //  and we whant to count how long is this sequence
+            if set.contains(i-1) == false {
+                // start of the sequence
+                var nextNum = i+1
+                while set.contains(nextNum) {
+                    nextNum += 1
+                }
+                longSeq = max(longSeq, nextNum-i)
+            }
+        }
+        
+        return longSeq
+        
+    }
+}
