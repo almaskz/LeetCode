10d5e870b105206b11d6b3e2fd22fdd7597e6d25
almaskz <almas.kairatuly@gmail.com>
2022-02-02 21:39:10 -0800

    Time: 165 ms (13.87%), Space: 16 MB (10.13%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.swift
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.swift
@@ -1,46 +1,36 @@
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
         var freqs = [Int: Int]()
-        
         for i in nums {
             freqs[i, default: 0] += 1
         }
-        
-        var list = Array(freqs.keys)
-        
-        var l = 0, r = list.count - 1
-        
-        /*
-        0 1 2
-        1 2 3
-          
-        [0...mid] [mid+1..]
-        */
-        
+
+        var list = Array(freqs.keys)	// [3 1 2]
+
+        var l = 0, r = list.count-1
+        // 0 1 2 3 4 5
+        // 1 2 3 4 5 6
+        // [...p][p+1…]
+
         while l < r {
-            // 
-            let mid = quickSelect(&list, l, r, freqs)
-            if list.count - mid < k {
-                r = mid - 1
-            } else if list.count - mid > k {
-                l = mid + 1
+            let pivot = quickSelect(freqs, &list, l, r)
+
+            if list.count - pivot < k {
+                r = pivot - 1
+            } else if list.count - pivot > k {
+                l = pivot + 1
             } else {
                 return Array(list[(list.count-k)...])
-            }
+            } 	
         }
         return Array(list[(list.count-k)...])
+
     }
-    
-    // [1,1,1,2,2,3]
-    // [1] = 3 ; [2] = 2 ; [3] = 1
-    //        0 1 2
-    // list = 3 2 1 k = 2   
-    
-    func quickSelect(_ arr: inout [Int], _ lo: Int, _ hi: Int, _ freqs: [Int: Int]) -> Int {
+     
+    func quickSelect(_ freqs: [Int: Int], _ arr: inout [Int], _ lo: Int, _ hi: Int) -> Int {
         let pivotIndex = Int.random(in: lo..<hi)
-        
-        arr.swapAt(hi, pivotIndex)
-        
+        arr.swapAt(pivotIndex, hi)
+
         var left = lo
         for i in lo..<hi {
             if freqs[arr[i]]! <= freqs[arr[hi]]! {
@@ -48,9 +38,8 @@ class Solution {
                 left += 1
             }
         }
-        
         arr.swapAt(left, hi)
-        
         return left
     }
+
 }
