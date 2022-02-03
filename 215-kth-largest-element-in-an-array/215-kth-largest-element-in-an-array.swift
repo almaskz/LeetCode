ab2f04830910818d346da848f3e4b4a943ccca1a
almaskz <almas.kairatuly@gmail.com>
2022-02-02 17:29:04 -0800

    Time: 57 ms (59.89%), Space: 14.4 MB (31.66%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.swift
@@ -0,0 +1,49 @@
+class Solution {
+    func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+        // 1  2 3 4 5 6
+        //                  p
+        // [3,6,4,5,5,3,2,1,2]  k = 4  >=
+        //                  i
+        //            l
+        // [..p] [p+1..]
+        /*
+         0 1 2 3 4 5
+        [3,2,1,5,6,4]
+        [3,2,4,5,6,1]
+        */
+        
+        var arr = nums
+        
+        func quickSelect(_ lo: Int, _ hi: Int) -> Int {
+            let pivotIndex = Int.random(in: lo..<hi)
+            //print("pivot = \(arr[pivotIndex])")
+            arr.swapAt(pivotIndex, hi)
+            var left = lo
+            for i in lo..<hi {
+                if arr[i] >= arr[hi] {
+                    arr.swapAt(i, left)
+                    left += 1
+                }
+            }
+            
+            arr.swapAt(hi, left)
+            return left
+        }
+        
+        var l = 0, r = nums.count - 1
+        
+        while l < r {
+            let target = quickSelect(l, r)
+            //print("target = \(target)")
+            //print(arr)
+            if target + 1 < k {
+                l = target + 1  
+            } else if target + 1 > k {
+                r = target - 1
+            } else {
+                return arr[target]
+            }
+        }
+        return arr[k-1]
+    }
+}
