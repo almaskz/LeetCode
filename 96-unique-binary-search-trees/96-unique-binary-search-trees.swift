e98fc389757c8757be8d6ffe6da655480fcc9de9
almaskz <almas.kairatuly@gmail.com>
2022-02-09 22:03:51 -0500

    Time: 0 ms (100.00%), Space: 14 MB (16.95%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func numTrees(_ n: Int) -> Int {
+        guard n > 1 else { return 1 }
+        var dp = Array(repeating: 0, count: n+1)
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in 2...n {
+            for j in 1...i {
+                dp[i] += (dp[j-1] * dp[i-j])
+            }
+        }
+        
+        return dp[n]
+    }
+}
