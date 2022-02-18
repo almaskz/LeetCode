01e31f8462fe3d56f9ecfd6fc386546c18c40079
almaskz <almas.kairatuly@gmail.com>
2022-02-17 20:36:20 -0500

    Time: 19 ms (41.88%), Space: 15 MB (23.75%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.swift
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.swift
@@ -13,29 +13,30 @@
  */
 
 class Solution {
-    var first: Node? = nil
-    var last: Node? = nil
     func treeToDoublyList(_ root: Node?) -> Node? {
-        guard let node = root else { return nil }
+        var first: Node?
+        var last: Node?
+        helper(root, &first, &last)
         
-        helper(node)
-        last?.right = first
         first?.left = last
+        last?.right = first
+        
         return first
     }
-    
-    func helper(_ node: Node?) {
-        if let node = node {
-            helper(node.left)
-            
-            if last != nil {
-                last?.right = node
-                node.left = last
-            } else {
-                first = node
-            }
-            last = node
-            helper(node.right)
-        } 
+    // 4, nil, nil <-
+    // 2, nil, nil 
+    func helper(_ node: Node?, _ first: inout Node?, _ last: inout Node?) {
+        guard let node = node else { return }
+        
+        helper(node.left, &first, &last)
+        
+        if last != nil {            // 
+            last?.right = node      // 1->right = 2 | 2->right = 3
+            node.left = last       // 2->left = 1  | 3->left = 2 
+        } else {
+            first = node            // first = 1 
+        }
+        last = node                 // last = 1 | 2  | 3 
+        helper(node.right, &first, &last)       // 3, 1, 2 
     }
 }
