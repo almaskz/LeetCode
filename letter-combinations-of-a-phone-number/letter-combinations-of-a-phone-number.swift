dc97548c3c3db5e19b9eded54bc66742a1570dd7
almaskz <almas.kairatuly@gmail.com>
2022-02-18 19:29:32 -0500

    Time: 8 ms, Memory: 14.3 MB - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.swift
@@ -0,0 +1,48 @@
+class Solution {
+    var ans = [String]()
+    func letterCombinations(_ digits: String) -> [String] {
+        guard digits.isEmpty == false else { return [] }
+        var comb = String()
+        let temp = Array(digits)
+        backtrack(temp, &comb, 0)
+        return ans
+    }
+    // 2 3
+    // i
+    // comb = ""
+    //  0 1
+    // [2 3], "", 0
+    // ans = ["ad"],  
+    func backtrack(_ nums: [Character], _ comb: inout String, _ digit: Int) {
+        if comb.count == nums.count {
+            ans.append(comb)
+            return 
+        }
+        var candidates = helper(nums[digit])       
+        for ch in candidates {              // a | d | e
+            comb.append(ch)                 // comb = ae
+            backtrack(nums, &comb, digit+1) // [23], "a", 1 | [23], "ad", 2 
+            comb.removeLast()               // comb = "a"
+        }
+    }
+    
+    private func helper(_ digit: Character) -> String {
+        if digit == "2" {
+            return "abc"
+        } else if digit == "3" {
+            return "def"
+        } else if digit == "4" {
+            return "ghi"
+        } else if digit == "5" {
+            return "jkl"
+        } else if digit == "6" {
+            return "mno"
+        } else if digit == "7" {
+            return "pqrs"
+        } else if digit == "8" {
+            return "tuv"
+        } else {
+            return "wxyz"
+        }
+    }
+}
