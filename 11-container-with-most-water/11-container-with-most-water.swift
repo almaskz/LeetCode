139296f0c6c075ff578bc3c23aca70db9b5e93f3
almaskz <almas.kairatuly@gmail.com>
2022-02-19 20:21:15 -0500

    Time: 918 ms (45.07%), Space: 18.4 MB (10.99%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.swift
+++ 11-container-with-most-water/11-container-with-most-water.swift
@@ -1,24 +1,23 @@
 class Solution {
     func maxArea(_ height: [Int]) -> Int {
+        var left = 0
+        var right = height.count-1
+        var maxWater = 0            // (right - left) * min(heights)
         // 0 1 2 3 4 5 6 7 8
         //[1,8,6,2,5,4,8,3,7]
-        //     l       r
-        // cur = 6*4=28
+        //   l
+        //               r
+        // cur = 7-1*3
         // max = 49
-        
-        var l = 0, r = height.count-1
-        var maxArea = 0 
-        
-        while l < r {
-            var cur = min(height[l], height[r]) * (r-l)
-            maxArea = max(cur, maxArea)
-            if height[l] < height[r] {
-                l += 1
+        while left < right {
+            let curWater = (right-left) * min(height[left], height[right])
+            maxWater = max(maxWater, curWater)
+            if height[left] < height[right] {
+                left += 1
             } else {
-                r -= 1
+                right -= 1
             }
         }
-        
-        return maxArea 
+        return maxWater
     }
 }
