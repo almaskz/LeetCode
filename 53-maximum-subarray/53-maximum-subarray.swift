df2e1b31b559367eab4a07d9d3f70aa21d3d79af
almaskz <almas.kairatuly@gmail.com>
2022-02-19 20:51:40 -0500

    Time: 803 ms (89.40%), Space: 18.8 MB (7.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.swift
+++ 53-maximum-subarray/53-maximum-subarray.swift
@@ -1,41 +1,25 @@
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        guard nums.count > 1 else { return nums[0] }
-        return maxSubArray(0, nums.count-1, nums)
-    }
-    
-    func maxSubArray(_ l: Int, _ r: Int, _ arr: [Int]) -> Int {
-        // base case 
-        if l >= r {             // l < r 
-            return arr[l]
-        }
+        //[-2 ]
+        //[-2,1,-3,4,-1,2,1,-5,4]
+        //         l           r
+        // cur 1
+        //
+        var maxSum = nums[0]                // -2
+        var l = 0, r = l + 1
         
-        let m = l+(r-l+1)/2                 // avoid overflow, m
-        let left = maxSubArray(l, m-1, arr) // 
-        let right = maxSubArray(m, r, arr)  // middle in the right side
-        let mid = countMid(l, m, r, arr)
-        
-        return max(left, right, mid)
-    }
-    
-    // calculate consistently to the way we split middle, from l -> m-1 and m -> r
-    func countMid(_ l: Int, _ m: Int, _ r: Int, _ arr: [Int]) -> Int {
-        var resultLeft = 0
-        if m-1 >= l {
-            var cur = arr[m-1]
-            resultLeft = cur
-            for i in stride(from: m-2, through: l, by: -1) {
-                cur += arr[i]
-                resultLeft = max(cur, resultLeft)
+        var curSum = maxSum                 // -2
+        while r < nums.count {              //
+            if curSum + nums[r] < nums[r] { // 
+                curSum = nums[r]            // cur = 4
+                l = r                       // 
+            } else {
+                curSum += nums[r]           // cur = 1
             }
-        } 
-        
-        var cur = arr[m]
-        var resultRight = cur
-        for i in stride(from: m+1, through: r, by: 1) {
-            cur += arr[i]
-            resultRight = max(resultRight, cur)
+            maxSum = max(maxSum, curSum)    // max = 6
+            r += 1
         }
-        return resultLeft+resultRight
+        
+        return maxSum
     }
 }
