3a86c906a8408d1e405658debf84843cf252a44f
almaskz <almas.kairatuly@gmail.com>
2022-02-21 17:44:24 -0500

    Time: 26 ms (16.38%), Space: 14.5 MB (92.27%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.swift
+++ 206-reverse-linked-list/206-reverse-linked-list.swift
@@ -10,42 +10,15 @@
  */
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
-        //   1<-2<-3<-4<-5   <- | ->
-        //nil<-1  2->3->4<-5->nil
-        //  p  c  n
-        //var prev: ListNode?
-        return iterative(head)
-    }
-    
-    func recursive(_ node: ListNode?, _ prev: ListNode?) -> ListNode? {
-        if node == nil {
-            return prev
-        }
-        var next = node?.next
-        node?.next = prev
-        return recursive(next, node)
-    }
-    
-    func iterative(_ node: ListNode?) -> ListNode? {
         var prev: ListNode? = nil
-        var cur = node
-        var next: ListNode?
+        var cur = head
+        
         while cur != nil {
-            next = cur?.next
+            let next = cur?.next
             cur?.next = prev
             prev = cur
             cur = next
         }
         return prev
-    }
-    
-    func helper(_ node: ListNode?) -> ListNode? {
-        if node == nil || node?.next == nil {
-            return node    
-        }
-        let last = helper(node?.next)
-        node?.next?.next = node
-        node?.next = nil
-        return last
     }
 }
