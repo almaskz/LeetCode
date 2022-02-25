5d326c88d97816b66f209b20f88a0d873d060665
almaskz <almas.kairatuly@gmail.com>
2022-02-25 14:49:01 -0500

    Time: 40 ms (82.07%), Space: 14.8 MB (38.88%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.swift
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.swift
@@ -10,22 +10,24 @@
  *         self.val = val
  *         self.left = left
  *         self.right = right
- *     }true 
+ *     }
  * }
  */
 class Solution {
     func isValidBST(_ root: TreeNode?) -> Bool {
-        var prev: Int? = nil
-        
-        func inorder(_ node: TreeNode?) -> Bool {
-            guard let node = node else { return true }
-
-            if inorder(node.left) == false { return false }
-            if prev != nil && prev! >= node.val { return false }
-            prev = node.val
-            return inorder(node.right)
+        return helper(root, nil, nil)
+    }
+    
+    func helper(_ node: TreeNode?, _ lo: Int?, _ hi: Int?) -> Bool {
+        guard let node = node else { return true }
+        if let left = lo, left >= node.val {
+            return false
+        } 
+        if let right = hi, right <= node.val {
+            return false
         }
-        
-        return inorder(root)
+        let left = helper(node.left, lo, node.val)
+        let right = helper(node.right, node.val, hi)
+        return left && right
     }
 }
