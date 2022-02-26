7fc9963abb256af15d9105d886c5cd81489524c3
almaskz <almas.kairatuly@gmail.com>
2022-02-26 15:29:06 -0500

    Time: 3 ms (54.55%), Space: 13.5 MB (92.73%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.swift
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.swift
@@ -1,15 +1,25 @@
 class Solution {
     func numTrees(_ n: Int) -> Int {
-        guard n > 1 else { return 1 }
+        /*
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = [1 2]
+        dp[3] = dp[1]*dp[3-1] + dp[2]*dp[1] + dp[3]*dp[1]
+        */
         var dp = Array(repeating: 0, count: n+1)
         dp[0] = 1
         dp[1] = 1
         
-        for i in 2...n {
-            for j in 1...i {
-                dp[i] += (dp[j-1] * dp[i-j])
+        
+        for node in stride(from: 2, through: n, by: 1) {
+            var total = 0
+            for root in 1...node {
+                let left = root - 1
+                let right = node - root
+                total += dp[left]*dp[right]
             }
-        }
+            dp[node] = total
+        } 
         
         return dp[n]
     }
