8a1a978b35cae091a609abd152a234ab4a3639cb
almaskz <almas.kairatuly@gmail.com>
2022-02-27 12:22:40 -0500

    Time: 295 ms (68.49%), Space: 15.1 MB (11.64%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.swift
+++ 875-koko-eating-bananas/875-koko-eating-bananas.swift
@@ -1,14 +1,11 @@
 class Solution {
     func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
-        // piles = [30,11,23,4,20], h = 5
-        // min = 1
-        // max = 30
         var lo = 1
         var hi = piles.max() ?? 0
         
         while lo < hi {
-            let mid = lo + (hi-lo)/2        // 1+(30-1)/2=9+1=10
-            if condition(mid, piles, h) {   // 
+            let mid = lo + (hi-lo)/2
+            if condition(mid, piles, h) {
                 hi = mid
             } else {
                 lo = mid + 1
@@ -17,12 +14,12 @@ class Solution {
         return lo
     }
     
-    // 10
     func condition(_ speed: Int, _ piles: [Int], _ h: Int) -> Bool {
-        var curHours = 0
+        var hoursSpend = 0
         for pile in piles {
-            curHours += Int(ceil(Double(pile)/Double(speed)))
-            if curHours > h {
+            var hoursNeeded = Int(ceil(Double(pile)/Double(speed)))
+            hoursSpend += hoursNeeded
+            if hoursSpend > h {
                 return false
             }
         }
