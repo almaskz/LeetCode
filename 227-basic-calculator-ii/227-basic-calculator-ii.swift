5b2c9c0fe80cd946bad2386d68eabaaf23e20659
almaskz <almas.kairatuly@gmail.com>
2022-03-01 17:31:46 -0500

    Time: 25 ms (55.22%), Space: 18.1 MB (41.29%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.swift
+++ 227-basic-calculator-ii/227-basic-calculator-ii.swift
@@ -1,46 +1,48 @@
 class Solution {
     func calculate(_ s: String) -> Int {
-        var lastNum = 0 
-        var curNum = 0
         var result = 0
-        var sign = "+"
+        var curNum = 0
+        var lastNum = 0
+    
+        var prevOp = "+"
+        
         var l = 0
         var arr = Array(s)
         //"3+2*2"
         //     l
-        // cur = 0
-        // [3 2]
-        // op = "*"
-        
+        // result = 3
+        // curNum = 2
+        // lastNum = 2
+        // prevOp = "*"
         while l < arr.count {
             if arr[l].isWholeNumber {
                 var num = 0
                 while l < arr.count, let digit = arr[l].wholeNumberValue {
-                    num = num*10 + digit
+                    num = num * 10 + digit
                     l += 1
                 }
                 l -= 1
                 curNum = num
-            } 
-            if  arr[l] == "*" || arr[l] == "/" ||
-                arr[l] == "+" || arr[l] == "-"  || 
-                l == arr.count - 1 {
-                if sign == "*" {
-                    lastNum *= curNum
-                } else if sign == "/" {
-                    lastNum /= curNum
-                } else if sign == "+" {
+            }
+            if arr[l].isWholeNumber == false && arr[l] != " " || l == arr.count - 1 {
+                if prevOp == "+" {
                     result += lastNum
                     lastNum = curNum
-                } else if sign == "-" {
+                } else if prevOp == "-" {
                     result += lastNum
-                    lastNum = -(curNum)
+                    lastNum = -curNum
+                } else if prevOp == "*" {
+                    lastNum *= curNum
+                } else if prevOp == "/" {
+                    lastNum /= curNum
                 }
-                sign = String(arr[l])
-                curNum = 0
+                
+                prevOp = String(arr[l])
+                //curNum = 0
             }
             l += 1
         }
+        
         result += lastNum
         return result
     }
