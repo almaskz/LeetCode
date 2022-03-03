6d266e7d813aa90560601e99b3fdda5bbb660c6a
almaskz <almas.kairatuly@gmail.com>
2022-03-03 18:37:24 -0500

    Time: 20 ms (75.00%), Space: 18.6 MB (19.23%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.swift
+++ 224-basic-calculator/224-basic-calculator.swift
@@ -4,45 +4,39 @@ class Solution {
         return helper(&index, Array(s))
     }
     
+    // (1+(4+5+2)-3)+(6+8)
+    //   i
+    // result =  
+    // sum = 1
+    // sign = 1
     func helper(_ index: inout Int, _ arr: [Character]) -> Int {
+        var sum = 0
         var sign = 1
-        var result = 0
-        var current = 0
-        
-        //"1 + 1"
-        // i
         
         while index < arr.count {
-            if arr[index].isWholeNumber {
-                var num = 0
-                while index < arr.count, let digit = arr[index].wholeNumberValue {
-                    num = num * 10 + digit
-                    index += 1
-                }
-                current = num
-                result += (current*sign)
-                current = 0
+            if arr[index] == "+" {
                 sign = 1
-            } else if arr[index] == "(" {
-                index += 1
-                current = helper(&index, arr)
-                result += (current*sign)
-                current = 0
-                sign = 1
-                index += 1
-            } else if arr[index] == ")" {
-                return result + (sign * current)
-            } else if arr[index] == "+" {
-                sign = 1
-                index += 1
             } else if arr[index] == "-" {
                 sign = -1
-                index += 1
-            } else {
-                index += 1    
+            } else if arr[index].isWholeNumber {
+                var num = 0
+                while index < arr.count, let digit = arr[index].wholeNumberValue {
+                    num = num*10 + digit
+                    index += 1
+                }
+                index -= 1
+                sum += (sign*num)
+            } else if arr[index] == "(" {
+                index += 1      // pass ( and recurse on the rest of array
+                let result = helper(&index, arr)
+                sum += (sign*result)
+            } else if arr[index] == ")" {
+                return sum
             }
+            index += 1
         }
-        return result
+        
+        return sum
     }
     
 }
