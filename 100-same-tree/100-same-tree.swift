1b0313c805784ff36c9cafcf05b0fc302b1faa5c
almaskz <almas.kairatuly@gmail.com>
2022-03-04 13:39:33 -0500

    Time: 7 ms (36.76%), Space: 13.9 MB (60.96%) - LeetHub

--- 100-same-tree/100-same-tree.swift
+++ 100-same-tree/100-same-tree.swift
@@ -15,12 +15,51 @@
  */
 class Solution {
     func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-        if p == nil && q == nil {
-            return true
-        } else if p?.val != q?.val {
-            return false
-        } else {
-            return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        return bfs(p, q)
+    }
+    
+    func bfs(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil && q == nil { return true }
+        guard let pNode = p, let qNode = q else { return false }
+        
+        var pQueue = [TreeNode]()
+        var qQueue = [TreeNode]()
+        
+        pQueue.append(pNode)
+        qQueue.append(qNode)
+        
+        while pQueue.isEmpty == false && qQueue.isEmpty == false {
+            if pQueue.count != qQueue.count { return false }
+            var nextP = [TreeNode]()
+            var nextQ = [TreeNode]()
+            for i in 0..<pQueue.count {
+                let curP = pQueue[i]
+                let curQ = qQueue[i]
+                if curP.val != curQ.val { return false }
+    
+                if (curP.left == nil) != (curQ.left == nil) {
+                    return false
+                }
+                if (curP.right == nil) != (curQ.right == nil) {
+                    return false
+                }
+                if let pLeft = curP.left {
+                    nextP.append(pLeft)
+                } 
+                if let qLeft = curQ.left {
+                    nextQ.append(qLeft)
+                }
+                if let pRight = curP.right {
+                    nextP.append(pRight)
+                } 
+                if let qRight = curQ.right {
+                    nextQ.append(qRight)
+                }
+            }
+            pQueue = nextP
+            qQueue = nextQ
         }
+        
+        return true
     }
 }
