71c7a122a23834eb1a26b51fd11358fcb075cf8a
almaskz <almas.kairatuly@gmail.com>
2022-03-04 13:05:29 -0500

    Time: 0 ms (100.00%), Space: 14.3 MB (17.35%) - LeetHub

--- 100-same-tree/100-same-tree.swift
+++ 100-same-tree/100-same-tree.swift
@@ -15,27 +15,12 @@
  */
 class Solution {
     func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-        var stack = [(p: TreeNode?, q: TreeNode?)]()
-        
-        stack.append((p: p, q: q))
-        
-        while(stack.isEmpty == false) {
-            let cur = stack.removeFirst()
-            
-            if cur.p != nil || cur.q != nil {
-                if check(cur) {
-                    stack.append((p: cur.p?.left, q: cur.q?.left))
-                    stack.append((p: cur.p?.right, q: cur.q?.right))
-                } else {
-                    return false
-                }
-            }
+        if p == nil && q == nil {
+            return true
+        } else if p?.val != q?.val {
+            return false
+        } else {
+            return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
         }
-        
-        return true
-    }
-    
-    func check(_ item: (p: TreeNode?, q: TreeNode?)) -> Bool {
-         return item.p?.val == item.q?.val
     }
 }
