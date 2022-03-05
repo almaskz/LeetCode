591f5dbfb2406821153c2e586b20210aa35bb0c4
almaskz <almas.kairatuly@gmail.com>
2022-03-05 15:25:06 -0500

    Time: 183 ms (24.05%), Space: 14.2 MB (46.45%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.swift
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.swift
@@ -17,23 +17,24 @@ class Solution {
     func isSubtree(_ root: TreeNode?, _ subRoot: TreeNode?) -> Bool {
         if subRoot == nil {
             return true
-        }
-        if root == nil {
+        } else if root == nil {
             return false
         }
+        
         if sameTree(root, subRoot) {
             return true
         } else {
-            return isSubtree(root?.left, subRoot) || isSubtree(root?.right, subRoot)
+            return (isSubtree(root?.left, subRoot) || 
+                    isSubtree(root?.right, subRoot))
         }
     }
     
     func sameTree(_ node1: TreeNode?, _ node2: TreeNode?) -> Bool {
         if node1 == nil && node2 == nil {
             return true
-        }
-        if node1?.val == node2?.val {
-            return (sameTree(node1?.left, node2?.left) && sameTree(node1?.right, node2?.right))
+        } else if node1?.val == node2?.val {
+            return (sameTree(node1?.left, node2?.left) && 
+                    sameTree(node1?.right, node2?.right))
         } else {
             return false
         }
