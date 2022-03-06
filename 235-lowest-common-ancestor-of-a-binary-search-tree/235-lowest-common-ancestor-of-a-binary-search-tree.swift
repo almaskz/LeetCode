6d7dafb0368d3079d35397eeeaf6aa1e960b34bd
almaskz <almas.kairatuly@gmail.com>
2022-03-05 23:35:15 -0500

    Time: 183 ms (39.39%), Space: 15.3 MB (8.33%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.swift
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.swift
@@ -15,6 +15,19 @@
 class Solution {
     func lowestCommonAncestor(_ root: TreeNode?,
                               _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        var cur = root 
+        
+        while cur != nil {
+            if cur!.val > p!.val && cur!.val > q!.val {
+                cur = cur?.left
+            } else if cur!.val < p!.val && cur!.val < q!.val {
+                cur = cur?.right
+            } else {
+                return cur
+            }
+        }
+        return cur
+        /*
         guard let node = root else { return nil }
         
         if node.val > p!.val && node.val > q!.val {
@@ -24,5 +37,6 @@ class Solution {
         } else {
             return node
         }     
+        */
     }
 }
