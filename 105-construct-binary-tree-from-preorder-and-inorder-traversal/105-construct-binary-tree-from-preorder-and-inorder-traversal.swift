06d045c7d19a66ef94cda98d9e1ce95e3bd69787
almaskz <almas.kairatuly@gmail.com>
2022-03-05 20:00:54 -0500

    Time: 51 ms (44.24%), Space: 85.1 MB (33.33%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.swift
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+        if preorder.isEmpty || inorder.isEmpty {
+            return nil
+        } 
+        let root = TreeNode(preorder[0])
+        let mid = inorder.firstIndex(of: preorder[0])!
+        if mid >= 1 {
+            root.left = buildTree(Array(preorder[1...mid]), Array(inorder[0...(mid-1)]))    
+        }
+        if mid + 1 < preorder.count {
+            root.right = buildTree(Array(preorder[(mid+1)...]), Array(inorder[(mid+1)...]))   
+        }
+        
+        return root
+    }
+    
+    
+}
