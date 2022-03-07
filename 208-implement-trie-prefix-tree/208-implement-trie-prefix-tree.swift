981c410e85111222d9cbcbe8c93c080fa9b6eb7e
almaskz <almas.kairatuly@gmail.com>
2022-03-06 21:46:22 -0500

    Time: 269 ms (71.10%), Space: 21.4 MB (41.04%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.swift
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.swift
@@ -1,82 +1,67 @@
+
 class Trie {
-    class TrieNode<T: Hashable> {
-        var value: T?
-        var children = [T: TrieNode]()
+    class TrieNode <T: Hashable> {
+        var val: T?
+        var adj = [T: TrieNode]()
         var isEnd = false
         
-        init(value: T? = nil) {
-            self.value = value
+        init(_ val: T? = nil) {
+            self.val = val
         }
         
-        func add(_ child: T) {
-            guard children[child] == nil else { return }
-            children[child] = TrieNode(value: child)
+        func add(_ child: T) -> TrieNode? {
+            guard adj[child] == nil else { return nil }
+            let node = TrieNode(child)
+            adj[child] = node
+            return node
         }
     }
     
     typealias Node = TrieNode<Character>
     var root: Node
-
+    
     init() {
         root = Node()
     }
     
     func insert(_ word: String) {
         guard word.isEmpty == false else { return }
-        
-        var currentNode = root
-
+        var cur = root
         for char in word {
-            if let child = currentNode.children[char] {
-                currentNode = child
-            } else {
-                currentNode.add(char)
-                currentNode = currentNode.children[char]!
+            if let node = cur.adj[char] {
+                cur = node
+            } else if let node = cur.add(char) {
+                cur = node
             }
         }
-        
-        currentNode.isEnd = true
+        cur.isEnd = true
     }
     
-    
     func search(_ word: String) -> Bool {
-        guard word.isEmpty == false else { return false }
-        
-        var currentNode = root
-        var currentIndex = 0
-        var arr = Array(word)
-        
-        while currentIndex < arr.count {
-            if let child = currentNode.children[arr[currentIndex]] {
-                currentNode = child
+        var cur = root
+        for char in word {
+            if let node = cur.adj[char] {
+                cur = node
             } else {
                 return false
             }
-            currentIndex += 1
         }
-        
-        return currentNode.isEnd
+        return cur.isEnd 
     }
     
     func startsWith(_ prefix: String) -> Bool {
-        guard prefix.isEmpty == false else { return false }
-        
-        var currentNode = root
-        var currentIndex = 0
-        var arr = Array(prefix)
-        
-        while currentIndex < arr.count {
-            if let child = currentNode.children[arr[currentIndex]] {
-                currentNode = child
+        var cur = root
+        for char in prefix {
+            if let node = cur.adj[char] {
+                cur = node
             } else {
                 return false
             }
-            currentIndex += 1
         }
-        
         return true
     }
 }
+
 /**
  * Your Trie object will be instantiated and called as such:
  * let obj = Trie()
