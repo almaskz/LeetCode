c5378bbacff7886821b4853d44a85ff6c95ad04e
almaskz <almas.kairatuly@gmail.com>
2022-03-08 15:32:20 -0500

    Time: 31 ms (58.59%), Space: 14.3 MB (86.87%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.swift
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.swift
@@ -12,16 +12,24 @@ class Solution {
         var dict = [Character: Int]()   // dict of the character frequences of the current window
         
         var l = 0
-        var maxF = 0
+
         for r in 0..<arr.count {
             dict[arr[r], default: 0] += 1
-            maxF = max(maxF, dict[arr[r]]!)
-            while ((r-l+1) - maxF) > k {
+            
+            while ((r-l+1) - maxF(dict)) > k {
                 dict[arr[l], default: 0] -= 1
                 l += 1
             }
             maxL = max(maxL, r-l+1)
         }
         return maxL
+    }
+        
+    private func maxF(_ dict: [Character: Int]) -> Int {
+        var maxF = 0
+        for value in dict.values {
+            maxF = max(maxF, value)
+        }
+        return maxF
     }
 }
