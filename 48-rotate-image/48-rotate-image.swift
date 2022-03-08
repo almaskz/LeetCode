9e1372b74e9b1f3e94cbf7af8aa97f32b2427586
almaskz <almas.kairatuly@gmail.com>
2022-03-08 13:02:18 -0500

    Time: 7 ms (90.55%), Space: 14.2 MB (24.09%) - LeetHub

--- 48-rotate-image/48-rotate-image.swift
+++ 48-rotate-image/48-rotate-image.swift
@@ -1,20 +1,18 @@
 class Solution {
     func rotate(_ matrix: inout [[Int]]) {
+        let n = matrix.count
         var left = 0
-        var right = matrix.count - 1
+        var right = n-1
         
         while left < right {
             var top = left
             var bottom = right
+            
             for i in 0..<(right-left) {
                 let topLeft = matrix[top][left+i]
-                
                 matrix[top][left+i] = matrix[bottom-i][left]
-                
                 matrix[bottom-i][left] = matrix[bottom][right-i]
-                
                 matrix[bottom][right-i] = matrix[top+i][right]
-                
                 matrix[top+i][right] = topLeft
             }
             left += 1
@@ -22,10 +20,3 @@ class Solution {
         }
     }
 }
-
-/*
-[[9,4,7],
- [8,5,2],
- [3,6,1]]
-
-*/
