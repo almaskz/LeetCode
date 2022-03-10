06e939bcb027f4bb4a14431726a1783dc365b748
almaskz <almas.kairatuly@gmail.com>
2022-03-10 15:18:34 -0500

    Time: 31 ms (91.76%), Space: 14.2 MB (76.44%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.swift
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.swift
@@ -1,23 +1,30 @@
 class Solution {
     func longestPalindrome(_ s: String) -> String {
-        var n = s.count
-        var dp = Array(repeating: Array(repeating: false, count: n), count: n)
-        var aL = 0
-        var aR = 0
         var arr = Array(s)
-        
-        for i in stride(from: n-1, through: 0, by: -1) {
-            for j in i..<n {
-                if arr[j] == arr[i] && (j-i < 3 || dp[i+1][j-1]) {
-                    dp[i][j] = true
-                    if j-i > aR-aL {
-                        aL = i
-                        aR = j
-                    }
-                }
+        var maxSide = (left: 0, right: 0)
+        for i in 0..<arr.count {
+            let evenPalindrom = longestPalindrom(i, i, arr)
+            let oddPalindrom = longestPalindrom(i, i+1, arr) 
+            if evenPalindrom.right - evenPalindrom.left > maxSide.right - maxSide.left {
+                maxSide = evenPalindrom
+            }
+            if oddPalindrom.right - oddPalindrom.left > maxSide.right - maxSide.left {
+                maxSide = oddPalindrom
             }
         }
         
-        return String(arr[aL...aR])
+        return String(arr[maxSide.left...maxSide.right])
+    }
+    
+    func longestPalindrom(_ left: Int, _ right: Int,  _ arr: [Character]) -> (left: Int, right: Int) {
+        var l = left, r = right
+        
+        // 1 - case i is center
+        // 2 - case arr[l] == arr[r]  
+        while l >= 0 && r < arr.count && arr[l] == arr[r] {
+            l -= 1
+            r += 1
+        }
+        return (left: l+1, right: r-1)
     }
 }
