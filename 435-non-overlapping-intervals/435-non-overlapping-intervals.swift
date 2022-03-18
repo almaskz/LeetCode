5596e0afd2c756e4698c4a340fd8750bf45a0c0a
almaskz <almas.kairatuly@gmail.com>
2022-03-18 17:31:06 -0400

    Time: 1800 ms (53.17%), Space: 23.3 MB (34.18%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.swift
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.swift
@@ -1,26 +1,24 @@
 class Solution {
     func eraseOverlapIntervals(_ intervals: [[Int]]) -> Int {
-        var counter = 0
-        guard intervals.count > 1 else { return counter } 
-
-        var arr = intervals.sorted(by:{ $0[0] < $1[0] })	// sort in-place by start time
-        print(arr)
-        var prev = 0 
-        var cur = 1
-        while cur < arr.count {
-            // check for overlap
-            if arr[cur][0] < arr[prev][1] {
-            // pick smaller end time
-                if arr[cur][1] < arr[prev][1] {
+        var sorted = intervals.sorted { $0[0] < $1[0] } // sort by start time   
+        
+        var prev = sorted[0]
+        var counter = 0    
+    
+        for i in 1..<sorted.count {
+            var cur = sorted[i]
+            // check overlap
+            if prev[1] > cur[0] {
+                counter += 1
+                // greedy way to pick the one with smaller end time
+                if cur[1] < prev[1] {
                     prev = cur
                 }
-                counter += 1
             } else {
-                prev = cur
+                prev = cur   
             }
-            cur += 1
         }
-
+        
         return counter
     }
 }
