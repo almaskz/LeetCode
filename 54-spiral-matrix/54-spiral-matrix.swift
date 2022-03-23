19dcb4f549e9e3c40fe5e1c6b7cce07db5addec3
almaskz <almas.kairatuly@gmail.com>
2022-03-23 12:28:30 -0400

    Time: 6 ms (29.26%), Space: 14 MB (73.70%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.swift
+++ 54-spiral-matrix/54-spiral-matrix.swift
@@ -1,43 +1,54 @@
 class Solution {
     func spiralOrder(_ matrix: [[Int]]) -> [Int] {
-        let m = matrix.count
-        let n = matrix[0].count
-        var left = 0
-        var right = matrix[0].count-1
+        let kN = matrix.count
+        let kM = matrix[0].count
         var top = 0
-        var bottom = matrix.count-1
+        var left = 0
+        var bottom =  kN-1
+        var right = kM-1
+        if bottom == 0 {
+            return matrix[0]
+        }
+        if right == 0 {
+            var ans = [Int]()
+            for row in matrix {
+                ans.append(row[0])
+            }
+            return ans
+        }
+        var ans = [Int]()
+        ans.reserveCapacity(kN*kM)
         
-        var result = [Int]()
-        result.reserveCapacity(m*n)
-        
-        // left = 0
-        // right = 1
         // top = 1
-        // bottom = 2
-        // [1 2 3 6 9]
-        while top <= bottom && left <= right {       
-            for i in stride(from: left, through: right, by: 1) {             // 0..2 | 
-                result.append(matrix[top][i])
+        // left = 0
+        // bottom = 1
+        // right = 2
+        // ans = [1, 2, 3, 4, 8, 12, 11, 10, 9 ]
+        while top <= bottom || left <= right {    // 
+            for i in stride(from: left, through: right, by: 1) {    // 0...3
+                ans.append(matrix[top][i])    
             }
             top += 1
-            
-            for i in stride(from: top, through: bottom, by: 1) {             // 1..2 |
-                result.append(matrix[i][right])
+            for i in stride(from: top, through: bottom, by: 1) {    // 1..2
+                ans.append(matrix[i][right])    //
             }
-            right -= 1                          
-            
-            guard top <= bottom && left <= right else { break }
-            
-            for i in stride(from: right, through: left, by: -1) {
-                result.append(matrix[bottom][i])
+            right -= 1
+            if ans.count == kN*kM {
+                break
+            }
+            for i in stride(from: right, through: left, by: -1) {   // 2..0
+                ans.append(matrix[bottom][i])       
             }
             bottom -= 1
-            
-            for i in stride(from: bottom, through: top, by: -1) {
-                result.append(matrix[i][left])
+            for i in stride(from: bottom, through: top, by: -1) {   // 1, 1
+                ans.append(matrix[i][left])
             }
             left += 1
+            if ans.count == kN*kM {
+                break
+            }
+            
         }
-        return result
+        return ans
     }
 }
