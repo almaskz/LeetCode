50dd524dce1927c66a62f2b3b19a08d23c667135
almaskz <almas.kairatuly@gmail.com>
2022-03-23 14:30:58 -0400

    Time: 133 ms (50.00%), Space: 14.6 MB (23.86%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.swift
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.swift
@@ -1,39 +1,38 @@
 class Solution {
     func setZeroes(_ matrix: inout [[Int]]) {
-        let kRows = matrix.count
-        let kCols = matrix[0].count
+        var topRow = false 
         
-        var zeroFirstRow = false
-        
-        for row in 0..<kRows {
-            for col in 0..<kCols {
+        for row in 0..<matrix.count {
+            for col in 0..<matrix[0].count {
                 if matrix[row][col] == 0 {
-                    matrix[0][col] = 0
-                    if row > 0 {
-                        matrix[row][0] = 0
+                    if row == 0 {
+                        topRow = true
                     } else {
-                        zeroFirstRow = true
-                    }
-                }
+                        matrix[row][0] = 0    
+                    } 
+                    matrix[0][col] = 0
+                }        
             }
         }
         
-        for row in 1..<kRows {
-            for col in 1..<kCols {
+        // check cols
+        for row in 1..<matrix.count {
+            for col in 1..<matrix[0].count {
                 if matrix[0][col] == 0 || matrix[row][0] == 0 {
                     matrix[row][col] = 0
-                }       
+                }  
             }
         }
         
         // handle 1st col
         if matrix[0][0] == 0 {
-            for row in 0..<kRows {
+            for row in 0..<matrix.count {
                 matrix[row][0] = 0
             }
         }
-        if zeroFirstRow {
-            for col in 0..<kCols {
+        
+        if topRow {
+            for col in 0..<matrix[0].count {
                 matrix[0][col] = 0
             }
         }
