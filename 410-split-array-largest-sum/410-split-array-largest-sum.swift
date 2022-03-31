f0b3ad7d60c8f4e646785f0fb737fffd35af6944
almaskz <almas.kairatuly@gmail.com>
2022-03-31 19:49:30 -0400

    Time: 5 ms (68.42%), Space: 13.8 MB (94.74%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.swift
+++ 410-split-array-largest-sum/410-split-array-largest-sum.swift
@@ -1,20 +1,25 @@
 class Solution {
     func splitArray(_ nums: [Int], _ m: Int) -> Int {
+        // [7,2,5,10,8], m = 2
+        
         var lo = nums.max() ?? 0
         var hi = nums.reduce(0, +)
         
         while lo < hi {
             let mid = lo + (hi-lo)/2
             if condition(mid, nums, m) {
-                hi = mid
+                hi = mid 
             } else {
-                lo = mid + 1
+                lo = mid + 1 
             }
         }
-        
         return lo
     }
     
+    // [7,2,5,10,8], m = 2 ; 18
+    //         l
+    // cur = 14
+    // 18 -> 7
     func condition(_ threshold: Int, _ nums: [Int], _ m: Int) -> Bool {
         var count = 1
         var curSum = 0
@@ -30,5 +35,4 @@ class Solution {
         }
         return true
     }
-    
 }
