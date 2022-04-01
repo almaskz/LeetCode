2b8472e2d3c19912d6e0c65d767a9f3775e40974
almaskz <almas.kairatuly@gmail.com>
2022-04-01 16:32:15 -0400

    Time: 363 ms (5.04%), Space: 21.8 MB (5.90%) - LeetHub

--- 344-reverse-string/344-reverse-string.swift
+++ 344-reverse-string/344-reverse-string.swift
@@ -1,14 +1,15 @@
 class Solution {
     func reverseString(_ s: inout [Character]) {
-        var left = 0
-        var right = s.count-1
-        
-        while left < right {
-            let temp = s[left]
-            s[left] = s[right]
-            s[right] = temp
-            left += 1
-            right -= 1
+        recursive(0, s.count-1, &s)
+    }
+    
+    func recursive(_ left: Int, _ right: Int, _ s: inout [Character]) {
+        if left >= right {
+            return    
         }
+        let temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        recursive(left+1, right-1, &s)
     }
 }
