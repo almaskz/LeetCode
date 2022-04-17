d1522c6ff9a5de452b2a337893b0666e84580d69
almaskz <almas.kairatuly@gmail.com>
2022-04-17 00:13:38 -0400

    Time: 790 ms (76.19%), Space: 15.9 MB (66.67%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.swift
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.swift
@@ -1,31 +1,18 @@
 class Solution {
     func eventualSafeNodes(_ graph: [[Int]]) -> [Int] {
-        //[[1,2],[2,3],[5],[0],[5],[],[]]
-        //              i
-        // i = 0
-        // safe = [0: false, 1: false, 2: true, 5: true,]
-        // result = []
-        
+        // - run dfs on graph with hashmap  
+        // 
         var safe = [Int: Bool]()
         var result = [Int]()
         
-        for i in 0..<graph.count {
-            if dfs(i, &safe, graph) {
-                result.append(i)
-            }
-        }
-        
-        return result
-    }
-    
-    func dfs(_ index: Int, _ safe: inout [Int: Bool], _ graph: [[Int]]) -> Bool {
+        func dfs(_ index: Int) -> Bool {
             if let value = safe[index] {
                 return value
             }
             safe[index] = false
             
             for nei in graph[index] {
-                if dfs(nei, &safe, graph) == false {
+                if dfs(nei) == false {
                     return false
                 }
             }
@@ -33,4 +20,13 @@ class Solution {
             safe[index] = true
             return true
         }
+        
+        for i in 0..<graph.count {
+            if dfs(i) {
+                result.append(i)
+            }
+        }
+        
+        return result
+    }
 }
