8657f768ba35da6a17080f0a4d8a6967c86297ff
almaskz <almas.kairatuly@gmail.com>
2022-04-27 14:25:27 -0400

    Time: 137 ms (24.50%), Space: 14.8 MB (53.85%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.swift
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.swift
@@ -1,21 +1,23 @@
 class Solution {
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
-        //[2,7,11,15]      target = 9
-        // l r          
-        // l+r = 9
-        //
+        //[2,7,11,15]
+        // l    r
+        // target = 9
+        // sum = 
+        var l = 0
+        var r = numbers.count-1
         
-        var l = 0, r = numbers.count-1
         while l < r {
-            let cur = numbers[l] + numbers[r]
-            if cur == target {
-                return [l+1, r+1]
-            } else if cur > target {
+            let sum = numbers[l] + numbers[r]
+            if sum < target {
+                l += 1
+            } else if sum > target {
                 r -= 1
             } else {
-                l += 1 
+                return [l+1, r+1]
             }
         }
+        
         return []
     }
 }
