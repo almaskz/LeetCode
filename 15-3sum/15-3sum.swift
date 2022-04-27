ce0c522b61d37c794e9d77f9445060a51a4a2dd8
almaskz <almas.kairatuly@gmail.com>
2022-04-27 15:10:15 -0400

    Time: 163 ms (67.41%), Space: 18.3 MB (26.52%) - LeetHub

--- 15-3sum/15-3sum.swift
+++ 15-3sum/15-3sum.swift
@@ -1,35 +1,35 @@
 class Solution {
-    func threeSum(_ nums: [Int]) -> [[Int]] {
-        //[-1,0,1,2,-1,-4] sum = 0
-        //[-4 -1 -1 0 1 2]
-        //        i l r   
-        // sum = -3
-        // [-1 -1 2] -> [-1 0 1] ->
-        
+    func threeSum(_ nums: [Int]) -> [[Int]] {        
+        //[-1,0,1,2,-1,-4]
+        //[-4, -1, -1, 0, 1, 2]
+        //  i    
+        //      l            
+        //                   r 
         guard nums.count > 2 else { return [] }
-        let sorted = nums.sorted()
+        var sorted = nums.sorted()
         var result = [[Int]]()
+
         for i in 0..<sorted.count-2 {
             if i > 0 && sorted[i] == sorted[i-1] {
                 continue
             }
-            var l = i+1, r = sorted.count-1
+            var l = i + 1
+            var r = sorted.count-1
             while l < r {
-                let threeSum = sorted[i] + sorted[l] + sorted[r]
-                if threeSum == 0 {
-                    result.append([sorted[i], sorted[l], sorted[r]])
+                let sum = sorted[i] + sorted[l] + sorted[r]
+                if sum > 0 {
+                    r -= 1
+                } else if sum < 0 {
                     l += 1
+                } else {
+                    result.append([sorted[i], sorted[l], sorted[r]])
+                    l += 1 
                     while l < r && sorted[l] == sorted[l-1] {
                         l += 1
                     }
-                } else if threeSum < 0 {
-                    l += 1                   
-                } else {
-                    r -= 1
-                }
+                }       
             }
         }
-        
         return result
     }
 }
