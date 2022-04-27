64be384be225216a7a7cf0175182425f935fc566
almaskz <almas.kairatuly@gmail.com>
2022-04-27 13:22:15 -0400

    Time: 51 ms (71.81%), Space: 14.3 MB (69.42%) - LeetHub

--- 1-two-sum/1-two-sum.swift
+++ 1-two-sum/1-two-sum.swift
@@ -1,22 +1,20 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        //nums = [2,7,11,15], target = 9
-        // 9-7 = 2 
+        //  0 1  2 3 
         // [2,7,11,15]
-        //    i 
-        // [2] = 0
-        // []
-        
-        var dict = [Int: Int]()
+        //  i 
+        // target = 9
+        // 
+        // map = [(),]
+        var map = [Int: Int]() // number and it's index
         for i in 0..<nums.count {
-            let diff = target - nums[i]
-            if let index = dict[diff] {
+            let val = target - nums[i]
+            if let index = map[val] {
                 return [i, index]
             } else {
-                dict[nums[i]] = i
+                map[nums[i]] = i 
             }
         }
-        
-        return [0,0]
+        return []
     }
 }
