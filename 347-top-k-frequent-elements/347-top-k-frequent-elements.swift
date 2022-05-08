1becd7ee800fe789adb836cb8b01bf3a5e74db8e
almaskz <almas.kairatuly@gmail.com>
2022-05-08 11:38:40 -0400

    Time: 74 ms (80.13%), Space: 15.7 MB (65.10%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.swift
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.swift
@@ -1,45 +1,29 @@
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
-        var freqs = [Int: Int]()
-        for i in nums {
-            freqs[i, default: 0] += 1
+        //[1,1,1,2,2,3]
+        let n = nums.count
+        var freqs = [Int: Int]() // the number and its freqency
+        for num in nums {
+            freqs[num, default: 0] += 1 
         }
-
-        var list = Array(freqs.keys)	// [3 1 2]
-
-        var l = 0, r = list.count-1
-        // 0 1 2 3 4 5
-        // 1 2 3 4 5 6
-        // [...p][p+1…]
-
-        while l < r {
-            let pivot = quickSelect(freqs, &list, l, r)
-
-            if list.count - pivot < k {
-                r = pivot - 1
-            } else if list.count - pivot > k {
-                l = pivot + 1
-            } else {
-                return Array(list[(list.count-k)...])
-            } 	
+        print("Here2")
+        // bucket sorting with the freqs
+        var bucket = Array(repeating: [Int](), count: n+1)
+        for elem in freqs {
+            bucket[elem.value].append(elem.key)
         }
-        return Array(list[(list.count-k)...])
-
-    }
-     
-    func quickSelect(_ freqs: [Int: Int], _ arr: inout [Int], _ lo: Int, _ hi: Int) -> Int {
-        let pivotIndex = Int.random(in: lo..<hi)
-        arr.swapAt(pivotIndex, hi)
-
-        var left = lo
-        for i in lo..<hi {
-            if freqs[arr[i]]! <= freqs[arr[hi]]! {
-                arr.swapAt(i, left)
-                left += 1
+        
+        var result = [Int]()
+        
+        for i in stride(from: n, through: 1, by: -1) {
+            if bucket[i].isEmpty == false {
+                result.append(contentsOf: bucket[i])
             }
+            
+            if result.count == k {
+                return result
+            }             
         }
-        arr.swapAt(left, hi)
-        return left
+        return result
     }
-
 }
