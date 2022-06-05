5b7becd80101f267bb163d322c8983d9e1dfbc61
almaskz <almas.kairatuly@gmail.com>
2022-06-05 15:53:52 +0600

    Time: 343 ms (42.86%), Space: 20.4 MB (14.29%) - LeetHub

--- 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.swift
+++ 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.swift
@@ -4,10 +4,11 @@ class Solution {
         let arr = Array(num)
         var half = Array(arr[0..<mid])
         //print("half: \(half)")
-
+        var visited = [Int](repeating: -1, count: 10)
         var pIndex = -1
         for i in stride(from: half.count-1, to: 0, by: -1) {
             //print("half[i] = \(half[i])")
+            visited[half[i].wholeNumberValue!] = i
             if half[i] > half[i-1] {
                 pIndex = i - 1
                 break
@@ -18,12 +19,13 @@ class Solution {
             return ""
         }
         var eIndexToSwap: Int = 0
-        for i in stride(from: half.count-1, to: pIndex, by: -1) {
-                if half[pIndex] < half[i] {
-                    eIndexToSwap = i
-                    break
+        for i in (half[pIndex].wholeNumberValue!+1)...9 {
+            if visited[i] != -1 {
+                eIndexToSwap = visited[i]
+                break
             }
         }
+        
         half.swapAt(eIndexToSwap, pIndex)
         let sorted = (half[(pIndex+1)...]).sorted()
         var final = half[0...pIndex] + sorted
