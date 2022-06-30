748137f95da9aaac088ca3f66f14d559d59a83fc
almaskz <almas.kairatuly@gmail.com>
2022-06-30 21:05:32 +0600

    Time: 89 ms (7.41%), Space: 14.9 MB (7.41%) - LeetHub

--- 1436-destination-city/1436-destination-city.swift
+++ 1436-destination-city/1436-destination-city.swift
@@ -1,21 +1,8 @@
 class Solution {
     func destCity(_ paths: [[String]]) -> String {
-        var set = Set<String>()
-        
-        for path in paths {
-            let from = path[0]
-            let to = path[1]
-            set.insert(to)
-            
-        }
-        for path in paths {
-            let from = path[0]
-            let to = path[1]
-            if set.contains(from) {
-                set.remove(from)
-            }
-        }
-        
-        return set.first!
+        var allFrom = Set(paths.map { $0[0] })
+        var allTo = Set(paths.map { $0[1] })
+    
+        return allTo.subtracting(allFrom).first!
     }
 }
