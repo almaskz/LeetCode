a96a5d947e64f9013980aadd929e46cb3d9fa3ef
almaskz <almas.kairatuly@gmail.com>
2022-06-30 19:42:51 +0600

    Time: 892 ms (7.14%), Space: 15.9 MB (92.86%) - LeetHub

---
+++ 1333-filter-restaurants-by-vegan-friendly-price-and-distance/1333-filter-restaurants-by-vegan-friendly-price-and-distance.swift
@@ -0,0 +1,45 @@
+class Solution {
+    struct Resturant: Comparable  {
+        let id: Int
+        let rating: Int
+        let veganFriendly: Int
+        let price: Int
+        let distance: Int
+        
+        static func < (lhs: Resturant, rhs: Resturant) -> Bool {
+            if lhs.rating != rhs.rating {
+                return lhs.rating > rhs.rating
+            } else {
+                return lhs.id > rhs.id
+            }
+        }
+        
+        /*
+        static func == (lhs: Resturant, rhs: Resturant) -> Bool {
+            return lhs.id == rhs.id
+        }*/
+    } 
+    
+    func filterRestaurants(_ restaurants: [[Int]], _ veganFriendly: Int, _ maxPrice: Int, _ maxDistance: Int) -> [Int] {
+        
+        var filted = [Resturant]()
+        for item in restaurants {
+            let rest = Resturant(
+                id: item[0], 
+                rating: item[1],
+                veganFriendly: item[2],
+                price: item[3],
+                distance: item[4]
+            )
+            if rest.price <= maxPrice && rest.distance <= maxDistance {
+                if (veganFriendly == 1 && rest.veganFriendly == veganFriendly) ||
+                    veganFriendly == 0 {
+                    filted.append(rest)      
+                }
+            }
+        }
+        
+        let sorted = filted.sorted { $0 < $1 }
+        return sorted.map { $0.id }
+    }
+}
